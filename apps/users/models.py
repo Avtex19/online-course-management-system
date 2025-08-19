d743b15572cecdd4a731eb443155b2bb07e310a2
Avtex19 <atukaberadze19@gmail.com>
2025-08-19 13:16:30 +0400

    feat: define user model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from common.enums import UserRole
+
+
+class User(AbstractUser):
+    role = models.CharField(
+        max_length=20,
+        choices=UserRole.choices(),
+        default=UserRole.STUDENT.value,
+    )
+
