070ef8ef302e751ddefc6b1171450560cce83402
Avtex19 <atukaberadze19@gmail.com>
2025-08-19 13:16:15 +0400

    feat(admin): enhance User admin add form and tidy fieldsets

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "role",
+        "is_staff",
+    )
+    list_filter = DjangoUserAdmin.list_filter + ("role",)
+
+    fieldsets = DjangoUserAdmin.fieldsets + (
+        ("Role info", {"fields": ("role",)}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "password1",
+                    "password2",
+                    "role",
+                ),
+            },
+        ),
+    )
