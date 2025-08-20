8058dea5bb8e131165302b345b7f72c748e7a1ee
Avtex19 <atukaberadze19@gmail.com>
2025-08-20 23:45:04 +0400

    feat(models): add enums for user roles, fields, and error messages
    =

--- common/enums.py
+++ common/enums.py
@@ -10,3 +10,20 @@ class UserRole(Enum):
         return [(role.value, role.name.capitalize()) for role in cls]
 
 
+class ErrorMessages(str, Enum):
+    PASSWORDS_DO_NOT_MATCH = "Passwords do not match"
+    EMAIL_REQUIRED = "The Email field must be set"
+    SUPERUSER_STAFF_REQUIRED = "Superuser must have is_staff=True."
+    SUPERUSER_PERMISSION_REQUIRED = "Superuser must have is_superuser=True."
+
+
+class UserFields(str, Enum):
+    EMAIL = "email"
+    IS_STAFF = "is_staff"
+    IS_SUPERUSER = "is_superuser"
+    IS_ACTIVE = "is_active"
+
+
+
+
+
