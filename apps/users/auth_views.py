cf487af1f90d69303eece42a65938f1134d352ed
Avtex19 <atukaberadze19@gmail.com>
2025-08-21 11:50:15 +0400

    feat: Add Login and Logout APIViews using authentication services

--- apps/users/auth_views.py
+++ apps/users/auth_views.py
@@ -1,21 +1,42 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-from .services import RegistrationWithTokensService
+from .services import RegistrationWithTokensService, LoginWithTokensService, LogoutService
 
 
 class RegisterView(APIView):
     permission_classes = [AllowAny]
-    
+
     def post(self, request):
         response_data, errors = RegistrationWithTokensService.execute(request.data)
-        
+
         if response_data:
             return Response(response_data, status=status.HTTP_201_CREATED)
-        
+
         return Response(errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class LoginView(APIView):
+    permission_classes = [AllowAny]
 
+    def post(self, request):
+        response_data, errors = LoginWithTokensService.execute(request.data)
+
+        if response_data:
+            return Response(response_data, status=status.HTTP_200_OK)
+
+        return Response(errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        response_data, errors = LogoutService.execute(request.data)
+
+        if response_data:
+            return Response(response_data, status=status.HTTP_200_OK)
+
+        return Response(errors, status=status.HTTP_400_BAD_REQUEST)
