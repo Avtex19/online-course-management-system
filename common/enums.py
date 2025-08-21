4c19275c4f5b5733077b6cb91b4a8331d0aee619
Avtex19 <atukaberadze19@gmail.com>
2025-08-21 11:26:53 +0400

    feat: implement success and error messages

--- common/enums.py
+++ common/enums.py
@@ -15,6 +15,15 @@ class ErrorMessages(str, Enum):
     EMAIL_REQUIRED = "The Email field must be set"
     SUPERUSER_STAFF_REQUIRED = "Superuser must have is_staff=True."
     SUPERUSER_PERMISSION_REQUIRED = "Superuser must have is_superuser=True."
+    INVALID_CREDENTIALS = "Invalid email or password"
+    INACTIVE_ACCOUNT = "Account is disabled"
+    REFRESH_TOKEN_REQUIRED = "Refresh token is required"
+
+
+class SuccessMessages(str, Enum):
+    LOGOUT_SUCCESS = "Successfully logged out"
+    REGISTRATION_SUCCESS = "Account created successfully"
+    LOGIN_SUCCESS = "Login successful"
 
 
 class UserFields(str, Enum):
