b542fec36abeb10178b91414ddfb95905cd6c544
Avtex19 <atukaberadze19@gmail.com>
2025-08-21 11:49:03 +0400

    feat: implement logout serializer for validating refresh token

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -5,7 +5,6 @@ from .models import User
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-
     password = serializers.CharField(write_only=True, min_length=8)
     password_confirm = serializers.CharField(write_only=True)
     role = serializers.ChoiceField(choices=UserRole.choices(), required=True)
@@ -22,7 +21,6 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         ]
 
     def validate(self, attrs):
-
         password = attrs.get("password")
         password_confirm = attrs.get("password_confirm")
         if password != password_confirm:
@@ -30,6 +28,17 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
                 "password_confirm": ErrorMessages.PASSWORDS_DO_NOT_MATCH
             })
         return attrs
+
+
+class LogoutSerializer(serializers.Serializer):
+
+    refresh_token = serializers.CharField()
+
+    def validate_refresh_token(self, value):
+
+        if not value:
+            raise serializers.ValidationError(ErrorMessages.REFRESH_TOKEN_REQUIRED)
+        return value
 
 
 class UserListSerializer(serializers.ModelSerializer):
@@ -43,3 +52,21 @@ class UserListSerializer(serializers.ModelSerializer):
             "role",
         ]
         read_only_fields = ["id"]
+
+
+class UserLoginSerializer(serializers.Serializer):
+
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        if not email or not password:
+            raise serializers.ValidationError({
+                "non_field_errors": ErrorMessages.INVALID_CREDENTIALS
+            })
+
+        return attrs
