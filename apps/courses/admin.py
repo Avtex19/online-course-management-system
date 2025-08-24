f126b1cfe60d44f24ce2d9873738b959adedf192
Avtex19 <atukaberadze19@gmail.com>
2025-08-24 22:34:22 +0400

    feat: add models in django admin

---
+++ apps/courses/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+from .models import Course, CourseTeacher, CourseStudent
+
+
+class CourseTeacherInline(admin.TabularInline):
+    model = CourseTeacher
+    extra = 1
+    autocomplete_fields = ["user"]
+
+
+class CourseStudentInline(admin.TabularInline):
+    model = CourseStudent
+    extra = 1
+    autocomplete_fields = ["user"]
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("name", "primary_owner", "created_at", "updated_at")
+    search_fields = ("name", "description")
+    list_filter = ("created_at", "updated_at")
+    inlines = [CourseTeacherInline, CourseStudentInline]
+
+
+@admin.register(CourseTeacher)
+class CourseTeacherAdmin(admin.ModelAdmin):
+    list_display = ("course", "user", "added_at")
+    search_fields = ("course__name", "user__email", "user__first_name", "user__last_name")
+    list_filter = ("added_at",)
+    autocomplete_fields = ("course", "user")
+
+
+@admin.register(CourseStudent)
+class CourseStudentAdmin(admin.ModelAdmin):
+    list_display = ("course", "user", "enrolled_at")
+    search_fields = ("course__name", "user__email", "user__first_name", "user__last_name")
+    list_filter = ("enrolled_at",)
+    autocomplete_fields = ("course", "user")
