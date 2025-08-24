1ce9e6915233926caf18724d77ed16a671d20b2d
Avtex19 <atukaberadze19@gmail.com>
2025-08-24 12:47:39 +0400

    feat(users): refactor UserViewSet to use UserService for grouped listing

--- apps/users/views/user.py
+++ apps/users/views/user.py
@@ -1,16 +1,19 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from apps.users.permissions import DenyBlacklistedToken
 from apps.users.models import User
 from apps.users.serializers import UserListSerializer
+from rest_framework.response import Response
+
+from apps.users.services.user_service import UserService
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
+    serializer_class = UserListSerializer
     permission_classes = [IsAuthenticated, DenyBlacklistedToken]
 
-    def get_serializer_class(self):
-        return UserListSerializer
-
-
+    def list(self, request, *args, **kwargs):
+        grouped_users = UserService.get_users_grouped_by_role()
+        return Response(grouped_users)
