fa5ab1b3043db481e09e959e475691b735a4f0e7
Avtex19 <atukaberadze19@gmail.com>
2025-08-24 12:58:05 +0400

    fix: use enum instead of magic string

--- apps/users/services/user_service.py
+++ apps/users/services/user_service.py
@@ -1,12 +1,13 @@
 from collections import defaultdict
 from apps.users.models import User
 from apps.users.serializers import UserListSerializer
+from common.enums import UserFields
 
 
 class UserService:
     @staticmethod
     def get_users_grouped_by_role():
-        users = User.objects.all().order_by("role")
+        users = User.objects.all().order_by(UserFields.ROLE.value)
         grouped = defaultdict(list)
 
         for user in users:
