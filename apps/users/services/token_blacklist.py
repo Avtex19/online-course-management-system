5dfd1759ea1d084ab1a0a78a3b10a8445be0308b
Avtex19 <atukaberadze19@gmail.com>
2025-08-24 13:05:49 +0400

    fix: change magic strings with enums

--- apps/users/services/token_blacklist.py
+++ apps/users/services/token_blacklist.py
@@ -3,56 +3,54 @@ from django.contrib.auth import get_user_model
 from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
 from rest_framework_simplejwt.token_blacklist.models import OutstandingToken, BlacklistedToken
 
+from common.enums import TokenFields
 
 User = get_user_model()
 
 
 class TokenBlacklistService:
 
-	@staticmethod
-	def blacklist_refresh_token(refresh_token_str):
+    @staticmethod
+    def blacklist_refresh_token(refresh_token_str):
 
-		try:
-			token = RefreshToken(refresh_token_str)
-			token.blacklist()
-			return True, None
-		except Exception as e:
-			return False, str(e)
+        try:
+            token = RefreshToken(refresh_token_str)
+            token.blacklist()
+            return True, None
+        except Exception as e:
+            return False, str(e)
 
 
 class AccessTokenBlacklistService:
 
-	@staticmethod
-	def blacklist(access_token_str):
+    @staticmethod
+    def blacklist(access_token_str):
 
-		try:
-			access_token = AccessToken(access_token_str)
-			jti = access_token['jti']
+        try:
+            access_token = AccessToken(access_token_str)
+            jti = access_token[TokenFields.JTI.value]
+            try:
+                outstanding = OutstandingToken.objects.get(jti=jti)
+            except OutstandingToken.DoesNotExist:
+                user = None
+                if access_token.get(TokenFields.USER_ID.value):
+                    try:
+                        user = User.objects.get(id=access_token[TokenFields.USER_ID.value])
+                    except User.DoesNotExist:
+                        user = None
+                created_at_aware = datetime.fromtimestamp(access_token[TokenFields.IAT.value], tz=dt_timezone.utc)
+                expires_at_aware = datetime.fromtimestamp(access_token[TokenFields.EXP
+                                                          .value], tz=dt_timezone.utc)
 
-			# Try to find existing OutstandingToken, create if missing
-			try:
-				outstanding = OutstandingToken.objects.get(jti=jti)
-			except OutstandingToken.DoesNotExist:
-				user = None
-				if access_token.get('user_id'):
-					try:
-						user = User.objects.get(id=access_token['user_id'])
-					except User.DoesNotExist:
-						user = None
-				created_at_aware = datetime.fromtimestamp(access_token['iat'], tz=dt_timezone.utc)
-				expires_at_aware = datetime.fromtimestamp(access_token['exp'], tz=dt_timezone.utc)
+                outstanding = OutstandingToken.objects.create(
+                    token=str(access_token_str),
+                    jti=jti,
+                    created_at=created_at_aware,
+                    expires_at=expires_at_aware,
+                    user=user,
+                )
 
-				outstanding = OutstandingToken.objects.create(
-					token=str(access_token_str),
-					jti=jti,
-					created_at=created_at_aware,
-					expires_at=expires_at_aware,
-					user=user,
-				)
-
-			BlacklistedToken.objects.get_or_create(token=outstanding)
-			return True, None
-		except Exception as e:
-			return False, str(e)
-
-
+            BlacklistedToken.objects.get_or_create(token=outstanding)
+            return True, None
+        except Exception as e:
+            return False, str(e)
