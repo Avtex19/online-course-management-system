d1d67b8ef643d74aab320a7da87b3b9724bbc51e
Avtex19 <atukaberadze19@gmail.com>
2025-08-24 23:44:40 +0400

    chore: add __all__ to expose course, user DTOs and migrations

---
+++ apps/courses/services/dtos/__init__.py
@@ -0,0 +1,10 @@
+from .course import CourseCreationRequest, CourseUpdateRequest, CourseValidationResult, CourseUpdateValidationResult
+from .user import UserValidationResult
+
+__all__ = [
+    'CourseCreationRequest',
+    'CourseUpdateRequest',
+    'CourseValidationResult',
+    'CourseUpdateValidationResult',
+    'UserValidationResult',
+]
