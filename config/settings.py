e7090cddedbd2bf77186e45374f4cc006a3df758
Avtex19 <atukaberadze19@gmail.com>
2025-08-26 15:49:25 +0400

    feat: add comment on django debug

--- config/settings.py
+++ config/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'dev-insecure-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', '1') == '1'
+# Debug mode is enabled for development and assignment purposes(otherwise i will not leave as it is now )
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
